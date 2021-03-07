df246ed1b7861f615dd3a75e5d4028a79233ff64
Roman <musinrr@gmail.com>
2021-03-07 14:40:42 +0000

    Find table range

--- src/TableFormatter.ts
+++ src/TableFormatter.ts
@@ -1,14 +1,23 @@
-//import * as cm from 'codemirror';
-
-
 module.exports = {
-	default: function(context: any) {
+	default: function(_context: any) {
 
 		const plugin = function(CodeMirror) {
-            CodeMirror.defineExtension('formatTable', function() {
+			CodeMirror.defineExtension('formatTable', function() {
+				const cursor = this.getCursor();
+				let startLine = cursor.line;
+				while (this.getLine(startLine).trimStart().charAt(0) === '|') startLine--;
+				startLine++;
 
+				let endLine = cursor.line;
+				while (this.getLine(endLine).trimStart().charAt(0) === '|') endLine++;
+				endLine--;
+				
+
+				this.extendSelection(
+					{line: startLine, ch: 0},
+					{line: endLine, ch: this.getLine(endLine).length});
             });
-		};
+		}
 
 		return {
 			plugin: plugin,
